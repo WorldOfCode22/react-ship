74b05256d5dd3b599b0be38587c7748c52c1fe33
Sloan <gwaltneys2@winthrop.edu>
2018-05-04 16:52:12 -0400

    placeholder for user creation

---
+++ src/graphql/resolvers/user-resolvers.ts
@@ -0,0 +1,10 @@
+// File that holds all of the user resolvers for query and mutation
+
+/**
+ * method to create a user
+ */
+const CreateUser = (parentVal: any, args: any) => {
+  return "yes";
+};
+
+export {CreateUser};
