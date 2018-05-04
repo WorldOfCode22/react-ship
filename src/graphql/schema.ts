f923ffde8c9395fd91bb5fb79ef6b67ef4ac60dd
Sloan <gwaltneys2@winthrop.edu>
2018-05-04 16:35:14 -0400

    added mutations to schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,7 +1,12 @@
+/**
+ * This file describes the graphql schema
+ */
 import {GraphQLSchema} from "graphql";
+import {Mutations} from "./mutations";
 import {RootQuery} from "./root";
 
 const Schema = new GraphQLSchema({
+  mutation: Mutations,
   query: RootQuery,
 });
 
