c501f6f7dec9a15164bb5bd0e68da9d398afbe85
Sloan <gwaltneys2@winthrop.edu>
2018-05-04 16:52:21 -0400

    added create user command

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -2,11 +2,22 @@
  * This is the root file for the grpahql mutations
  */
 
- import {GraphQLObjectType} from "graphql";
+ import {GraphQLObjectType, GraphQLString} from "graphql";
+ import {CreateUser} from "./resolvers/user-resolvers";
+ import {UserType} from "./types/user-type";
 
  const Mutations = new GraphQLObjectType({
    fields: {
-
+    createUser: {
+      args: {
+        password: {type: GraphQLString},
+        username: {type: GraphQLString},
+      },
+      resolve(parentVal, args) {
+        return CreateUser(parentVal, args);
+      },
+      type: UserType,
+    },
    },
    name: "Mutations",
  });
