49142161489d2edd6d31af03ac38139716ba7bd0
Sloan <gwaltneys2@winthrop.edu>
2018-05-04 16:34:56 -0400

    added mutations file

---
+++ src/graphql/mutations.ts
@@ -0,0 +1,14 @@
+/**
+ * This is the root file for the grpahql mutations
+ */
+
+ import {GraphQLObjectType} from "graphql";
+
+ const Mutations = new GraphQLObjectType({
+   fields: {
+
+   },
+   name: "Mutations",
+ });
+
+ export {Mutations};
