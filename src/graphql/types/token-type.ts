9afe8e95629b1664a984a0f6923e1c32b560096a
Sloan <gwaltneys2@winthrop.edu>
2018-05-05 02:16:53 -0400

    added keys to token type

--- src/graphql/types/token-type.ts
+++ src/graphql/types/token-type.ts
@@ -1,9 +1,11 @@
 // File that describes a token to graphql
 
-import {GraphQLObjectType, GraphQLString} from "graphql";
+import {GraphQLInt, GraphQLObjectType, GraphQLString} from "graphql";
 
 const TokenType = new GraphQLObjectType({
   fields: {
+    exp: {type: GraphQLInt},
+    iat: {type: GraphQLInt},
     token: {type: GraphQLString},
   },
   name: "Token",
