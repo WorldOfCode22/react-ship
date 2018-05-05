1f993a5b8427875446012a9fc487a1256d9ae398
Sloan <gwaltneys2@winthrop.edu>
2018-05-05 00:45:31 -0400

    added placeholder user field

--- src/graphql/root.ts
+++ src/graphql/root.ts
@@ -2,10 +2,16 @@
  * This is the root GraphQL File that holds the query schema for the GraphQLSchema Object
  */
 import {GraphQLObjectType} from "graphql";
+import { UserType } from "./types/user-type";
 
 const RootQuery = new GraphQLObjectType({
   fields: {
+    User: {
+      args: {
 
+      },
+      type: UserType,
+    },
   },
   name: "Root_Query",
 });
