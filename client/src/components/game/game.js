6a5e982b3794a2587bfb465742ad19bdc5aac8d3
Sloan <gwaltneys2@winthrop.edu>
2018-05-05 11:33:32 -0400

    added game top level component

---
+++ client/src/components/game/game.js
@@ -0,0 +1,18 @@
+// File that layouts the game component
+// @flow
+import React, {Component} from "react";
+type Props = {}
+type State = {};
+/**
+ * The top level component for game functions
+ */
+class Game extends Component<Props, State> {
+  state = {};
+  render () {
+    return (
+      <h1>Game Page</h1>
+    )
+  }
+}
+
+export default Game
