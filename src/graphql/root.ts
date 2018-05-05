56c3c0cf643c81bbad0d507420c34e0ccac76721
Sloan <gwaltneys2@winthrop.edu>
2018-05-05 02:16:41 -0400

    added token to root maybe removed in future

--- src/graphql/root.ts
+++ src/graphql/root.ts
@@ -1,11 +1,22 @@
 /**
  * This is the root GraphQL File that holds the query schema for the GraphQLSchema Object
  */
-import {GraphQLObjectType} from "graphql";
+import {GraphQLNonNull, GraphQLObjectType, GraphQLString} from "graphql";
+import { getToken } from "./resolvers/token-resolvers";
+import { TokenType } from "./types/token-type";
 import { UserType } from "./types/user-type";
 
 const RootQuery = new GraphQLObjectType({
   fields: {
+    Token: {
+      args: {
+        token: {type: new GraphQLNonNull(GraphQLString)},
+      },
+      resolve(parentVal, args) {
+        return getToken(parentVal, args);
+      },
+      type: TokenType,
+    },
     User: {
       args: {
 
