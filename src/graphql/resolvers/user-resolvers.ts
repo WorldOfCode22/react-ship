b690efb5946d7158219ca396fdd6fb6efb81dac2
Sloan <gwaltneys2@winthrop.edu>
2018-05-05 00:45:48 -0400

    added create user method

--- src/graphql/resolvers/user-resolvers.ts
+++ src/graphql/resolvers/user-resolvers.ts
@@ -1,10 +1,16 @@
 // File that holds all of the user resolvers for query and mutation
-
+import { DatabaseHelper } from "../../lib/database-helper";
+import {IUserModel, User} from "../../mongo/user";
 /**
  * method to create a user
  */
-const CreateUser = (parentVal: any, args: any) => {
-  return "yes";
+const CreateUser = async (parentVal: any, args: any): Promise<IUserModel> => {
+  try {
+    const user = await DatabaseHelper.newUser(args.username, args.password);
+    return user;
+  } catch (e) {
+    throw e;
+  }
 };
 
 export {CreateUser};
