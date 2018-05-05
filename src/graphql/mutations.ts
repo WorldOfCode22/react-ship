6269da84cd393efeb8d23f8366bb1ea904e87741
Sloan <gwaltneys2@winthrop.edu>
2018-05-05 01:52:55 -0400

    added token field to mutations

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -2,24 +2,37 @@
  * This is the root file for the grpahql mutations
  */
 
- import {GraphQLObjectType, GraphQLString} from "graphql";
- import {CreateUser} from "./resolvers/user-resolvers";
- import {UserType} from "./types/user-type";
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString} from "graphql";
+import { DatabaseHelper } from "../lib/database-helper";
+import {createToken} from "./resolvers/token-resolvers";
+import {CreateUser} from "./resolvers/user-resolvers";
+import {TokenType} from "./types/token-type";
+import {UserType} from "./types/user-type";
 
- const Mutations = new GraphQLObjectType({
-   fields: {
-    createUser: {
-      args: {
-        password: {type: GraphQLString},
-        username: {type: GraphQLString},
-      },
-      resolve(parentVal, args) {
-        return CreateUser(parentVal, args);
-      },
-      type: UserType,
+const Mutations = new GraphQLObjectType({
+  fields: {
+  createToken: {
+    args: {
+      password: {type: new GraphQLNonNull(GraphQLString)},
+      username: {type: new GraphQLNonNull(GraphQLString)},
     },
-   },
+    resolve(parentVal, args) {
+      return createToken(parentVal, args);
+    },
+    type: TokenType,
+  },
+  createUser: {
+    args: {
+      password: {type: GraphQLString},
+      username: {type: GraphQLString},
+    },
+    resolve(parentVal, args) {
+      return CreateUser(parentVal, args);
+    },
+    type: UserType,
+    },
+  },
    name: "Mutations",
  });
 
- export {Mutations};
+export {Mutations};
