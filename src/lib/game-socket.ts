8ef68e5371402f599796ad5995f1ed148f668108
Sloan <gwaltneys2@winthrop.edu>
2018-05-06 19:00:24 -0400

    class for game sockets

---
+++ src/lib/game-socket.ts
@@ -0,0 +1,44 @@
+import socketIo from "socket.io";
+import { IEmittedMessage } from "./emitted-message-interface";
+import { IEmittedError } from "./errors/emitted-error-interface";
+import { CouldNotJoinRoom, SocketError } from "./errors/socket-error";
+import { IOHelper } from "./io-helper";
+/**
+ * class that handles the waiting and game rooms of the / namespace logic for socket.io sockets.
+ */
+class GameSocket {
+  private socket: socketIo.Socket;
+  constructor(socket: socketIo.Socket) {
+    this.socket = socket;
+  }
+
+  private socketConfig() {
+    this.socket.on("WAITING", () => {
+      this.socketWaiting();
+    });
+  }
+
+  private socketWaiting() {
+    try {
+    IOHelper.joinRoom(this.socket, "Waiting");
+    const joinMessage: IEmittedMessage = {message: "Waiting for a match"};
+    this.socket.emit("Joined Waiting Room", joinMessage);
+    } catch (e) {
+      if (e instanceof CouldNotJoinRoom) {
+        const emittedError: IEmittedError = {
+          error: "You could not join room",
+          reason: "Internal error",
+        };
+        this.socket.emit("Error", emittedError);
+      } else {
+        const emittedError: IEmittedError = {
+          error: "An unexpected error occurred",
+          reason: "Internal error",
+        };
+        this.socket.emit("Error", emittedError);
+      }
+    }
+  }
+}
+
+export {GameSocket};
