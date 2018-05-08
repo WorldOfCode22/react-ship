4fc0ea5497c8ed3fc086a1b96b4500722fed1081
Sloan <gwaltneys2@winthrop.edu>
2018-05-07 22:25:29 -0400

    added routes

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -2,7 +2,10 @@
 // @flow
 import React from "react";
 import {Route, Switch} from 'react-router-dom';
+import AuthCheck from "./auth/auth-check";
 import Game from "../components/game/game";
+import Login from "../components/login/login";
+import Home from "./home/home";
 
 /**
  * The body render of the react page created by a router selection
@@ -10,7 +13,9 @@ import Game from "../components/game/game";
 const Main = () => {
   return (
     <Switch>
-      <Route exact path="/Game" component={Game}/>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/game" render={() => <AuthCheck nextComponent={Game} />} />
+      <Route exact path="/login" component={Login}/>
     </Switch>
   )
 }
