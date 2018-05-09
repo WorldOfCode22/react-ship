da12260173d25856a78c7ddf23ed22fb32d16791
Sloan <gwaltneys2@winthrop.edu>
2018-05-09 00:32:33 -0400

    little more login logic

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,11 +1,50 @@
 // @flow
 
+import {Row, Col, InputGroup, Input, Button} from "reactstrap"
+import {ApplicationState} from "../../App"
 import React from "react";
 
 const login = () => {
-  return (
-    <h1>Login Page</h1>
+  return(
+    <Row>
+      <Col>
+        {loginForm()}
+      </Col>
+      <Col>
+        {registerForm()}
+      </Col>
+    </Row>
   )
 };
 
+const loginForm = (props: any) => {
+  return (
+    <ApplicationState.Consumer>
+      {state => 
+        <div>
+          <InputGroup>
+            <Input value={state.login.usernameValue} onChange={()=> {}} placeholder="Username"/>
+          </InputGroup>
+        </div>
+      }
+    </ApplicationState.Consumer>
+  )
+}
+
+const registerForm = () => {
+  return (
+    <div>
+      <h3>Register Here</h3>
+      <InputGroup>
+        <Input placeholder="Username"/>
+      </InputGroup>
+      <br />
+      <InputGroup>
+        <Input placeholder="Password" />
+      </InputGroup>
+      <br />
+      <Button color="primary">Register!</Button>
+    </div>
+)
+}
 export default login;
