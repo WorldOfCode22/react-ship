7f80cb998119e867970f112fbbc4068126b6f268
Sloan <gwaltneys2@winthrop.edu>
2018-05-09 00:32:08 -0400

    added app context

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,38 @@
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import React from 'react'
+import {Container} from "reactstrap"
 import Main from './components/main'
 import {isDev} from './env'
+import {LoginState, loginDefaults} from "./components/login/login-type"
 
 // setup Apollo
 const client = new ApolloClient({
   uri: isDev === true ? 'http:localhost:80/graphql' : '/graphql' 
 });
+
+export const ApplicationState = React.createContext({
+  login: loginDefaults
+});
+type State = {
+  login: LoginState
+}
 /**
  * Top level of all react functions
  */
-const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div>
-        <Main />
-      </div>
-    </ApolloProvider>
-  )
+class App extends React.Component<null, State> {
+  state = {login: loginDefaults};
+  render () {
+    return (
+      <ApolloProvider client={client}>
+        <ApplicationState.Provider value={this.state}>
+          <Container>
+            <Main />
+          </Container>
+        </ApplicationState.Provider>
+      </ApolloProvider>
+    )
+  }
 }
 
 export default App
